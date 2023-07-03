fb75ee57c91527f014035d390d66aca10107ab81
Rocio Greco <rochigreco95@gmail.com>
2023-07-03 18:35:01 -0300

    Agregada funcion vista

--- project/config/views.py
+++ project/config/views.py
@@ -4,3 +4,7 @@ def saludo(request):
 	return HttpResponse("Hola Django - Coder")
 def saludo_vista(request):
 	return HttpResponse("<h1>Segunda vista</h1>")
+def nombre(request, nombre: str, apellido: str):
+	nombre= nombre.capitalize()
+	apellido=apellido.capitalize()
+	return HttpResponse(f"{apellido},{nombre}")
