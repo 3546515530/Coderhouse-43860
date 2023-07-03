4de99cf5dbd471cc897b5129688e042c46825285
Rocio Greco <rochigreco95@gmail.com>
2023-07-03 18:51:24 -0300

    final de clase 17

--- project/config/views.py
+++ project/config/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-
+from django.template import Context,Template
 def saludo(request):
 	return HttpResponse("Hola Django - Coder")
 def saludo_vista(request):
@@ -8,3 +8,11 @@ def nombre(request, nombre: str, apellido: str):
 	nombre= nombre.capitalize()
 	apellido=apellido.capitalize()
 	return HttpResponse(f"{apellido},{nombre}")
+
+def probando_template(request):
+	mi_html=open("./templates/template1.html")
+	mi_template=Template(mi_html.read())
+	mi_html.close()
+	mi_contexto=Context()
+	mi_documento=mi_template.render(mi_contexto)
+	return HttpResponse(mi_documento)
