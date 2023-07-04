cdc046048e1f3a2f863879c3b50291c8d50fa5af
Rocio Greco <rochigreco95@gmail.com>
2023-07-04 18:34:39 -0300

    templates

--- project/apps/producto/views.py
+++ project/apps/producto/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    contexto={"aplicacion": "Producto"}
+    return render (request,"producto/index.html",contexto)
