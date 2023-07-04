e5a8192a74209664650188430354af689c7e26aa
Rocio Greco <rochigreco95@gmail.com>
2023-07-04 17:46:12 -0300

    Agregado de app: cliente,producto y home

--- project/config/settings.py
+++ project/config/settings.py
@@ -13,8 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import sys
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+APPS_DIR=BASE_DIR/"apps"
+sys.path.append(str(APPS_DIR))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -37,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cliente',
+    'producto',
+    'home',
 ]
 
 MIDDLEWARE = [
