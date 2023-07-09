09fde4875067c47c710d16ee70bef37b65cd4625
Rocio Greco <rochigreco95@gmail.com>
2023-07-09 10:31:25 -0300

    tercer entrega

--- project/apps/cliente/models.py
+++ project/apps/cliente/models.py
@@ -4,12 +4,22 @@ class Pais(models.Model):
     nombre = models.CharField(max_length=50)
     def __str__(self):
         return self.nombre
+    
+class Arbol(models.Model):
+    # Define los campos de tu modelo
+    especie = models.CharField(max_length=100)
+    cantidad = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.especie} {self.cantidad}"
+    
 class Cliente(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     nacimiento = models.DateField(null=True)
     pais_origen_id = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True,blank=True)
+    especie = models.CharField(Arbol,max_length=100,null=True,blank=True)
+    cantidad = models.IntegerField(Arbol,null=True,blank=True)
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
--- project/apps/cliente/views.py
+++ project/apps/cliente/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render, redirect
-from .models import Cliente, Pais
+from .models import Cliente, Pais, Arbol
 from .forms import ClienteForm
 from django.http import HttpRequest,HttpResponse
 from datetime import date
+
+
 # Create your views here.
 def home(request):
     clientes_registros=Cliente.objects.all()
     contexto={"clientes": clientes_registros}
     return render (request,"cliente/index.html",contexto)
+
+
 def crear_clientes(request):
     
+    e1=Arbol(especie="Paraiso")
+    e2=Arbol(especie="Algarrobo")
+    e3=Arbol(especie="Sauce")
+    e1.save()
+    e2.save()
+    e3.save()
+
     p1=Pais(nombre="Peru")
     p2=Pais(nombre="Mexico")
     p3=Pais(nombre="El salvador")
@@ -17,10 +28,10 @@ def crear_clientes(request):
     p2.save()
     p3.save()
 
-    c1=Cliente(nombre="Almendra", apellido="Ruiseñor",nacimiento=date(2015,1,1),pais_origen_id=p1)
-    c2=Cliente(nombre="Giordana", apellido="Tapelo",nacimiento=date(2005,2,2),pais_origen_id=p2)
-    c3=Cliente(nombre="Macarena", apellido="Lito",nacimiento=date(1990,1,1),pais_origen_id=p3)
-    c4=Cliente(nombre="Jhiordana", apellido="Perez",nacimiento=date(2005,1,1),pais_origen_id=None)
+    c1=Cliente(nombre="Rocio", apellido="Ruiseñor",nacimiento=date(2015,1,1),pais_origen_id=p1,especie="Algarrobo",cantidad=4)
+    c2=Cliente(nombre="Giordana", apellido="Tapelo",nacimiento=date(2005,2,2),pais_origen_id=p2,especie="Paraiso",cantidad=5)
+    c3=Cliente(nombre="Macarena", apellido="Lito",nacimiento=date(1990,1,1),pais_origen_id=p3, especie="Algarrobo",cantidad=15)
+    c4=Cliente(nombre="Anabella", apellido="Perez",nacimiento=date(2005,1,1),pais_origen_id=None,especie="Sauce",cantidad=6)
     c1.save()
     c2.save()
     c3.save()
@@ -37,6 +48,7 @@ def crear_cliente(request: HttpRequest)-> HttpResponse:
         form=ClienteForm() #Muestra campos para que el usuario cargue datos
 
     return render(request,"cliente/crear.html",{"form":form})
+
 def busqueda(request):
     #Busqueda por nobre que contenga "dana"
     cliente_nombre= Cliente.objects.filter(nombre__contains="dana")
@@ -47,10 +59,13 @@ def busqueda(request):
     #Fechas sin pais de origen
     cliente_pais=Cliente.objects.filter(pais_origen_id=None)
     
-    
+    #Clientes que quieran plantar Algarrobos
+    cliente_especie=Cliente.objects.filter(especie="Algarrobo")
+
     contexto={
         "clientes_nombre" : cliente_nombre,
         "clientes_nacimiento" : cliente_nacimiento,
         "clientes_pais" : cliente_pais,
+        "clientes_especie" : cliente_especie,
         }
     return render(request,"cliente/search.html",contexto)
