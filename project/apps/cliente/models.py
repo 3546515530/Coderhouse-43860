915a8d3395a8ce85b190ac19158f89b447093884
Rocio Greco <rochigreco95@gmail.com>
2023-07-11 21:54:08 -0300

    gtgt

--- project/apps/cliente/models.py
+++ project/apps/cliente/models.py
@@ -4,23 +4,12 @@ class Pais(models.Model):
     nombre = models.CharField(max_length=50)
     def __str__(self):
         return self.nombre
-    
-class Arbol(models.Model):
-    # Define los campos de tu modelo
-    especie = models.CharField(max_length=100)
-    cantidad = models.IntegerField()
 
-    def __str__(self):
-        return f"{self.especie} {self.cantidad}"
-    
 class Cliente(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     nacimiento = models.DateField(null=True)
-    email=models.CharField(max_length=50)
-    pais_destino_id = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True,blank=True)
-    especie = models.CharField(Arbol,null=True,blank=True)
-    cantidad = models.IntegerField(Arbol,null=True,blank=True)
+    pais_origen_id = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True,blank=True)
 
     def __str__(self):
         return f"{self.nombre} {self.apellido}"
