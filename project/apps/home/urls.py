9eccbddeefb9bd888e9e4a03de0eef12a8296bf0
Rocio Greco <rochigreco95@gmail.com>
2023-07-17 19:48:26 -0300

    About

--- project/apps/home/urls.py
+++ project/apps/home/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import home
 from .import views
 from django.contrib.auth.views import LogoutView
+from django.views.generic import TemplateView
 
 
 
@@ -12,6 +13,7 @@ urlpatterns = [
     path("",views.home,name="home"),
     path('login/', views.login_request, name="login"),
     path('logout/', LogoutView.as_view(template_name="Home/logout.html"), name="logout"),
+    path('about/', TemplateView.as_view(template_name="home/about.html"),name="about"),
 
 
 
